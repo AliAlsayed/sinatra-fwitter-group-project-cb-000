b291360321aa9a06cf55f0f197bd79ab50b0b444
Ali Alsayed <a_alsayed87@hotmail.com>
2019-03-05 10:24:03 +0000

    Automatically backed up by Learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,41 @@
 class TweetsController < ApplicationController
+  get '/tweets' do
+    if Helpers.logged_in?(session)
+      @tweets = Tweet.all
+      erb :'tweets/tweets'
+    else
+      redirect '/login'
+    end
+  end
+
+  get '/tweets/new' do
+    if Helpers.logged_in?(session)
+      erb :'tweets/new'
+    else
+      redirect '/login'
+    end
+  end
+
+  post '/tweets' do
+    if Helpers.logged_in?(session)
+      if !params[:content].empty?
+        Helpers.current_user(session).tweets << Tweet.create(content: params[:content])
+      else
+        redirect '/tweets/new'
+      end
+    else
+      redirect '/login'
+    end
+  end
+
+  get '/tweets/:id' do
+    if Helpers.logged_in?(session)
+      @tweet = Tweet.find(params[:id])
+      erb :'tweets/show_tweet'
+    else
+      redirect '/login'
+    end
+  end
 
 
 end
